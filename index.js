7d7d941a1aef88112a6dea0a4cc37e623a561ab9
jyeemvhs <jeffreyyee@vistausd.org>
2023-03-02 20:10:35 -0800

    Update index.js

--- index.js
+++ index.js
@@ -7,12 +7,10 @@ var routes = require("./routes");   //new
 let app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use('/', express.static('./'));   //new
+
 app.use('/js', express.static('./public/js'));   //new
 app.use(routes);  //new
 
-
-//app.use('/', express.static('./'));   //new
 
 //req is info sending to server from client.
 //res is info sending to client from server.
